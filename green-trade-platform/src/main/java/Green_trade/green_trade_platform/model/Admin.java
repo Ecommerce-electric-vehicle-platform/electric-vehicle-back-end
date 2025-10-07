b04b3f71f0b1e2bd5ed4f6af30c44e9805572b1e
duycfvn9@gmail.com <duycfvn9@gmail.com>
2025-10-07 21:36:55 +0700

    feat(post-listing): add Admin entity

---
+++ green-trade-platform/src/main/java/Green_trade/green_trade_platform/model/Admin.java
@@ -0,0 +1,49 @@
+package Green_trade.green_trade_platform.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false, unique = false)
+    private String password;
+
+    @Column(name = "full_name", nullable = false, unique = false)
+    private String fullName;
+
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(name = "is_super_admin", nullable = false, unique = false)
+    private String isSuperAdmin;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "status", nullable = false, unique = false)
+    private boolean status;
+
+    @Column(name = "created_at", nullable = false, unique = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false, unique = false)
+    private LocalDateTime updatedAt;
+
+}
