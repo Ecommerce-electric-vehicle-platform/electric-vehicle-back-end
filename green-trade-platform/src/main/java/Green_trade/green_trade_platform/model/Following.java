690badcdbe030b12707bc77cc52f6762c6a27a6f
duycfvn9@gmail.com <duycfvn9@gmail.com>
2025-10-20 00:02:23 +0700

    add a lot of Entity part 2

---
+++ green-trade-platform/src/main/java/Green_trade/green_trade_platform/model/Following.java
@@ -0,0 +1,39 @@
+package Green_trade.green_trade_platform.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "buyer")
+public class Following {
+    @EmbeddedId
+    private FollowingId id;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer_id")
+    @MapsId("buyerId")
+    @JsonManagedReference
+    private Buyer buyer;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    @JoinColumn(name = "seller_id")
+    @JsonManagedReference
+    private Seller seller;
+
+    @Column(name = "followed_at", nullable = false, unique = false)
+    private LocalDateTime followedAt;
+
+    @Column(name = "unfollowed_at", nullable = false, unique = false)
+    private LocalDateTime unfollowedAt;
+}
