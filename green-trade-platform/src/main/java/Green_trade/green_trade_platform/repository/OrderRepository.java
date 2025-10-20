253eb066031bc0596b8495077900a3d4f6f63ebe
duycfvn9@gmail.com <duycfvn9@gmail.com>
2025-10-20 20:17:29 +0700

    feat(raise-dispute): fetch order history API

--- green-trade-platform/src/main/java/Green_trade/green_trade_platform/repository/OrderRepository.java
+++ green-trade-platform/src/main/java/Green_trade/green_trade_platform/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package Green_trade.green_trade_platform.repository;
 
+import Green_trade.green_trade_platform.model.Buyer;
 import Green_trade.green_trade_platform.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByBuyer(Buyer buyer, Pageable pageable);
 }
