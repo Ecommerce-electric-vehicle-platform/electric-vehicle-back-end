dc59b25212e5c1008dcf0904f0f50b84a68180a4
duycfvn9@gmail.com <duycfvn9@gmail.com>
2025-10-24 13:20:26 +0700

    feat(place-order): update nullable ghnShopId field

--- green-trade-platform/src/main/java/Green_trade/green_trade_platform/model/Seller.java
+++ green-trade-platform/src/main/java/Green_trade/green_trade_platform/model/Seller.java
@@ -43,7 +43,7 @@ public class Seller {
     @Column(name = "store_name", nullable = false)
     private String storeName;
 
-    @Column(name = "ghn_shop_id", nullable = false, unique = false)
+    @Column(name = "ghn_shop_id", nullable = true, unique = false)
     private String ghnShopId;
 
     @Column(name = "store_policy_url", nullable = false, unique = true)
