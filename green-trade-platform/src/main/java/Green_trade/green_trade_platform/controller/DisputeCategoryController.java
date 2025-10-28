0d7d2350930c645cbf44af038f3a7b87599e6d15
duycfvn9@gmail.com <duycfvn9@gmail.com>
2025-10-28 15:36:38 +0700

    feat(place-order): add dispute category list api

--- green-trade-platform/src/main/java/Green_trade/green_trade_platform/controller/DisputeCategoryController.java
+++ green-trade-platform/src/main/java/Green_trade/green_trade_platform/controller/DisputeCategoryController.java
@@ -1,7 +1,9 @@
 package Green_trade.green_trade_platform.controller;
 
+import Green_trade.green_trade_platform.mapper.DisputeCategoryMapper;
 import Green_trade.green_trade_platform.mapper.ResponseMapper;
 import Green_trade.green_trade_platform.model.DisputeCategory;
+import Green_trade.green_trade_platform.response.DisputeCategoryResponse;
 import Green_trade.green_trade_platform.response.RestResponse;
 import Green_trade.green_trade_platform.service.implement.DisputeCategoryServiceImpl;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,19 +21,23 @@ public class DisputeCategoryController {
 
     private final DisputeCategoryServiceImpl disputeCategoryService;
     private final ResponseMapper responseMapper;
+    private final DisputeCategoryMapper disputeCategoryMapper;
 
-    public DisputeCategoryController(DisputeCategoryServiceImpl disputeCategoryService, ResponseMapper responseMapper) {
+    public DisputeCategoryController(DisputeCategoryServiceImpl disputeCategoryService, ResponseMapper responseMapper, DisputeCategoryMapper disputeCategoryMapper) {
         this.disputeCategoryService = disputeCategoryService;
         this.responseMapper = responseMapper;
+        this.disputeCategoryMapper = disputeCategoryMapper;
     }
 
     @GetMapping("/dispute-categories")
-    public ResponseEntity<RestResponse<?, ?>> getAllDisputeCategory() {
+    public ResponseEntity<RestResponse<List<DisputeCategoryResponse>, Object>> getAllDisputeCategory() {
+        List<DisputeCategoryResponse> responseData = new ArrayList<>();
         List<DisputeCategory> disputeCategories = disputeCategoryService.getAllDisputeCategory();
-        RestResponse<?, ?> response = responseMapper.toDto(
+        disputeCategories.forEach(disputeCategory -> responseData.add(disputeCategoryMapper.toDto(disputeCategory)));
+        RestResponse<List<DisputeCategoryResponse>, Object> response = responseMapper.toDto(
                 true,
                 "FETCH DISPUTE CATEGORY LIST",
-                null,
+                responseData,
                 null
         );
         return ResponseEntity.status(HttpStatus.OK.value()).body(response);
